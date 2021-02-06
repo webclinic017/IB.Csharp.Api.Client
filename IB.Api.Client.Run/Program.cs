061db42beade34f399d6d865dc941c23b66c6d0e
oneangrytrader <oneangrytrader@gmail.com>
2021-02-06 14:53:18 +0000

    .

---
+++ IB.Api.Client.Run/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IB.Api.Client.Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
