9ffe2290b226d2f94f17eb6c7bb23f5f8ba86491
oneangrytrader <oneangrytrader@gmail.com>
2021-02-28 10:46:20 +0000

    .

--- IB.Api.Client/Endpoint/Accounts/Response/AccountsResponse.cs
+++ IB.Api.Client/Endpoint/Accounts/Response/AccountsResponse.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
-using IB.Api.Client.Endpoint.Trades.Model;
+using IB.Api.Client.Endpoint.Accounts.Model;
 
 namespace IB.Api.Client.Endpoint.Accounts.Response
 {
-    public class AccountsResponse : List<Trade>
+    public class AccountsResponse : List<Account>
     {
     }
 }
