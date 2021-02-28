559a45507c362edcbe330b6a69f2ceb285e9b815
oneangrytrader <oneangrytrader@gmail.com>
2021-02-28 05:03:33 +0000

    .

---
+++ IB.Api.Client.Test/SessionTests.cs
@@ -0,0 +1,25 @@
+using System;
+using IB.Api.Client.Endpoint.Session;
+using NUnit.Framework;
+
+namespace IB.Api.Client.Test
+{
+    [TestFixture]
+    public class SessionTests
+    {
+        SessionEndpoint sessionEndpoint;
+        
+        [SetUp]
+        public void Setup()
+        {
+            sessionEndpoint = new SessionEndpoint();
+        }
+
+        [Test]
+        public void ValidateSession()
+        {
+            var response = sessionEndpoint.ValidateSession();
+            Assert.IsTrue(response.Result);
+        }
+    }
+}
